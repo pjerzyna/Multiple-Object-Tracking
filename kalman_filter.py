90177338cfdc300146dad6a5dc4e869d11455fc0
pjerzyna <pawel.jerzyna@interia.pl>
2026-04-08 23:52:49 +0200

    Kalman modification

--- kalman_filter.py
+++ kalman_filter.py
@@ -10,36 +10,3 @@ def create_kalman_filter(bbox):
 
     kf.x[:4] = np.array(bbox).reshape((4, 1))
     return kf
-
-# def create_kalman_filter(bbox):
-#     """
-#         state vector: [x, y, w, h, vx, vy]4
-#     """
-
-#     kf = KalmanFilter(dim_x=6, dim_z=4)
-
-#     kf.x[:4] = np.array(bbox).reshape((4, 1))
-
-#     kf.F = np.array([
-#         [1,0,0,0,1,0],
-#         [0,1,0,0,0,1],
-#         [0,0,1,0,0,0],
-#         [0,0,0,1,0,0],
-#         [0,0,0,0,1,0],
-#         [0,0,0,0,0,1],
-#     ])
-
-#     kf.H = np.array([
-#         [1,0,0,0,0,0],
-#         [0,1,0,0,0,0],
-#         [0,0,1,0,0,0],
-#         [0,0,0,1,0,0],
-#     ])
-
-#     kf.P *= 10
-
-#     kf.R *= 1
-
-#     kf.Q *= 0.01
-
-#     return kf
